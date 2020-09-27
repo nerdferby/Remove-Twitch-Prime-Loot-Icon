75220a95e036167ad429ad9afc7423db60c70035
Ferby <scottcummins3356@gmail.com>
2020-09-27 16:16:46 +0100

    Adding link to GitHub repo

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Remove Twitch Prime Loot icon
-// @namespace    http://tampermonkey.net/
-// @version      1.0
+// @namespace    https://github.com/nerdferby/Remove-Twitch-Prime-Loot-Icon
+// @version      1.0.1
 // @description  Removes the Prime Loot icon from Twitch
 // @author       Scott Cummins (Ferby)
 // @match        https://www.twitch.tv/*
